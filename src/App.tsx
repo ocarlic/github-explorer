2e37f412e134c7562927fee71ce935fbdb8c5745
Victor Carli <victorcarlibr@gmail.com>
2020-04-22 09:05:14 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </>
+)
+
+export default App;
